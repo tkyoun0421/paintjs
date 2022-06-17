d152b1d1c7d29129d54040c7f49e9a9ec7f0feda
Taegwan Youn <tkyoun0421@naver.com>
2022-06-18 00:11:17 +0900

    Canvas Events

---
+++ app.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector("#js-canvas");
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+}
+
+function onMouseDown(event) {
+    console.log(event);
+    painting = true;
+}
+
+function onMouseUp(event) {
+   stopPainting();
+}
+
+function onMouseLeave(event) {
+    stopPainting();
+}
+
+if (canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", onMouseLeave);
+}
