7852ef4edb14e5365b28033f8993fdd206a8d836
Taegwan Youn <tkyoun0421@naver.com>
2022-06-21 12:50:36 +0900

    !Recap

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("#js-canvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
