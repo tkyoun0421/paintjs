2c2ab990b9608f2f01851ffaee30a9285df46fb8
Taegwan Youn <tkyoun0421@naver.com>
2022-06-21 12:45:24 +0900

    2D Context

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const canvas = document.querySelector("#js-canvas");
-// asdadklasjdlkasjd
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
 let painting = false;
 
 function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-    console.log(event);
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-   stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(event) {
+    painting = true;
 }
 
 function onMouseLeave(event) {
@@ -26,7 +36,7 @@ function onMouseLeave(event) {
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", onMouseLeave);
 }
